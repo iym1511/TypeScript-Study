525e24edafa3cde944d1cff64d5c3b256b8c52a7
문일윤 <iym1511@naver.com>
2023-04-05 13:37:42 +0900

    test

--- src/page/NaverApi.tsx
+++ src/page/NaverApi.tsx
@@ -22,7 +22,7 @@ const NaverApi = () => {
 
     const [apiData,setApiData] = useState<apiType[]>([])
 
-    // searchDate= 에서 newDate 를 사용하여 변수로 넣어서 실시간처럼 바꿔줄수도있음
+    // searchDate= 에서 newDate 를 사용하여 변수로 넣어서 실시간처럼 바꿔줄수도있음 a
     const api = async (): Promise<void> =>{
         try{
             const response = await axios.get('http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate=2023-04-04&returnType=json&serviceKey=T8f%2Bzud6ZCplc0UNr9TV7YXgtE9csbbJ0hveDrVp1zN74Z8Vo86VCeXCpvQCKD8vyWP6kcTb87sq%2Bt%2F5W%2BXi%2Fw%3D%3D&numOfRows=100&pageNo=1');
