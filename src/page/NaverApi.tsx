e3775202f557a08f3785978deeb6472d27c266d1
문일윤 <mun-il-yun@mun-il-yun-ui-MacBookAir.local>
2023-04-05 13:34:37 +0900

    테스트업로드

--- src/page/NaverApi.tsx
+++ src/page/NaverApi.tsx
@@ -25,14 +25,26 @@ const NaverApi = () => {
     // searchDate= 에서 newDate 를 사용하여 변수로 넣어서 실시간처럼 바꿔줄수도있음
     const api = async (): Promise<void> =>{
         try{
-            const response = await axios.get('http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate=2023-04-02&returnType=json&serviceKey=T8f%2Bzud6ZCplc0UNr9TV7YXgtE9csbbJ0hveDrVp1zN74Z8Vo86VCeXCpvQCKD8vyWP6kcTb87sq%2Bt%2F5W%2BXi%2Fw%3D%3D&numOfRows=100&pageNo=1');
-            setApiData(response.data.response.body.items[0].informGrade);
+            const response = await axios.get('http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate=2023-04-04&returnType=json&serviceKey=T8f%2Bzud6ZCplc0UNr9TV7YXgtE9csbbJ0hveDrVp1zN74Z8Vo86VCeXCpvQCKD8vyWP6kcTb87sq%2Bt%2F5W%2BXi%2Fw%3D%3D&numOfRows=100&pageNo=1');
+            setApiData(response.data.response.body.items);
         } catch(error) {
             alert("Error")
         }
 
     }
     console.log(apiData);
+
+    // const api = async (): Promise<void> =>{
+    //     try{
+    //         const response = await axios.get('http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=서울&pageNo=1&numOfRows=100&returnType=json&serviceKey=T8f%2Bzud6ZCplc0UNr9TV7YXgtE9csbbJ0hveDrVp1zN74Z8Vo86VCeXCpvQCKD8vyWP6kcTb87sq%2Bt%2F5W%2BXi%2Fw%3D%3D&numOfRows=100&pageNo=1&ver=1.0');
+    //         setApiData(response.data.response.body.items);
+    //         console.log(response)
+    //     } catch(error) {
+    //         alert("Error")
+    //     }
+
+    // }
+    // console.log(apiData);
 
     useEffect(()=>{
         api()
@@ -44,10 +56,10 @@ const NaverApi = () => {
     return ( 
         <div>
             {
-                apiData.map((a: any)=>(
-                    <div>
+                apiData.map((a :apiType)=>(
+                    <div style={{border:"1px solid red"}}>
                         <img src={a.imageUrl1} alt="" />
-                        <p>{a.informGrade}</p>
+                        <p>{a.dataTime}</p>
                     </div>
                 ))
             }
