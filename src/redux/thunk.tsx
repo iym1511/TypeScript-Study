6c75c2a027d09675c44977898ef5d1f37b5626c3
문일윤 <iym1511@naver.com>
2023-04-16 22:23:19 +0900

    redux-thunk study

--- src/redux/thunk.tsx
+++ src/redux/thunk.tsx
@@ -1,5 +1,5 @@
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 interface apiType{
     imageUrl4: string,
@@ -34,8 +34,8 @@ interface initialStateType{
 
 export const asyncFetch = createAsyncThunk(
     'thunkSlice/asyncFetch',
-    async () => {
-        const response = await axios.get<Post>('http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate=2023-04-16&returnType=json&serviceKey=T8f%2Bzud6ZCplc0UNr9TV7YXgtE9csbbJ0hveDrVp1zN74Z8Vo86VCeXCpvQCKD8vyWP6kcTb87sq%2Bt%2F5W%2BXi%2Fw%3D%3D&numOfRows=100&pageNo=1');
+    async ():Promise<apiType[]> => { // return되는값이 apiType[] 이기때문
+        const response = await axios.get<Post, AxiosResponse<Post>>('http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustFrcstDspth?searchDate=2023-04-16&returnType=json&serviceKey=T8f%2Bzud6ZCplc0UNr9TV7YXgtE9csbbJ0hveDrVp1zN74Z8Vo86VCeXCpvQCKD8vyWP6kcTb87sq%2Bt%2F5W%2BXi%2Fw%3D%3D&numOfRows=100&pageNo=1');
         return response.data.response.body.items
     }
 )
@@ -55,13 +55,12 @@ const thunkSlice = createSlice({
         builder.addCase(asyncFetch.pending, (state, action): void => {
             state.status = 'loading';
         });
-        builder.addCase(asyncFetch.fulfilled, (state, action): void => {
+        builder.addCase(asyncFetch.fulfilled, (state, action:PayloadAction<apiType[]>): void => {
             state.data = action.payload;
             state.status = 'complete';
-            console.log(action.payload)
         });
-        builder.addCase(asyncFetch.rejected, (state, action): void => {
-            state.status = 'fail';
+        builder.addCase(asyncFetch.rejected, (state): void => {
+            state.status = 'error';
         });
     },
 })
